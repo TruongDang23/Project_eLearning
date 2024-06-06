f224912c41c3c1f9087c9d55861e31266fac07fe
TruongDang23 <quangtruong050123@gmail.com>
2024-06-06 16:55:17 +0700

    Đã xong phần backend cho Login

--- server/index.js
+++ server/index.js
@@ -1,29 +1,18 @@
+//import some library for handle http
+const cors = require('cors')
+
 const express = require('express')
 const app = express()
 const port = 3000
 
 // Import routes
 const userRoutes = require('./routes/userRoutes')
+const systemRoutes = require('./routes/systemRoutes')
 
 // Use routes
-app.use('/api', userRoutes) // All user routes will have a prefix of /api
-
-// Home route
-app.get('/', (req, res) => {
-  let mysql = require('mysql2')
-  let connection = mysql.createConnection({
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password:'truong050123',
-    database: 'projectelearning'
-  })
-
-  connection.connect(function(err) {
-    if (err) throw err
-    console.log("Connected!")
-  })
-})
+app.use('/u', userRoutes) // All user routes will have a prefix of /u
+app.use('/s', systemRoutes) // All system routes will have a prefix of /s
+app.use(cors())
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`)
