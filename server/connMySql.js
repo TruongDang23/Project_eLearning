84f3ff94c6d2590835c1d2db1d5acc2a6c46d3b2
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-07-08 17:50:24 +0700

    "Quizz" Branch

--- server/connMySql.js
+++ server/connMySql.js
@@ -1,17 +1,17 @@
-const mysql = require('mysql2')
+const mysql = require("mysql2");
 // Sử dụng kỹ thuật pooling để tạo tối đa 10 connection đến mysql
 // Các connection sẽ được luân phiên sử dụng.
 // Hạn chế việc connect liên tục đến database, đảm bảo hiệu suất ctrinh
 
 const connectMysql = mysql.createPool({
-  host: 'localhost',
-  port: '3306',
-  user: 'root',
-  password: 'truong050123', //Truong 1: truong050123 //Truong 2: root //Vinh: 12345
-  database: 'projectelearning',
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "12345", //Truong 1: truong050123 //Truong 2: root //Vinh: 12345
+  database: "projectelearning",
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
-})
+  queueLimit: 0,
+});
 
-module.exports = connectMysql
+module.exports = connectMysql;
