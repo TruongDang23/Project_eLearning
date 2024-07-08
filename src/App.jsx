d7de17504ce9c701f79b0e0ca554ae619789fc40
TruongDang23 <quangtruong050123@gmail.com>
2024-07-08 08:44:11 +0700

    Merge code from GD_HocKhoaHoc to Master

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
 import System from "./screens/system"
 import { Admin } from "./screens/admin"
 import { Instructor } from "./screens/teacher"
 import { Student } from "./screens/student"
-import { Course } from './screens/course'
-=======
-import System from "./screens/system";
-import { Admin } from "./screens/admin";
-import { Instructor } from "./screens/teacher";
-import { Student } from "./screens/student";
-import InforCourse from "./screens/system/inforCourse/InforCourse";
-import InforCourseData from "~/data/InforCourseData";
-import AccessCourseData from "./data/AccessCourseData";
-import AccessCourse from "./screens/student/accessCourse/AccessCourse";
->>>>>>> f247ec661b9bab9ba1cba87f857761002ab54c88
+import { Course } from "./screens/course"
+import AccessCourseData from "./data/AccessCourseData"
+import AccessCourse from "./screens/student/accessCourse/AccessCourse"
 
 function App() {
   return (
@@ -21,14 +12,9 @@ function App() {
       <System/>
       <Admin/>
       <Instructor/>
-<<<<<<< HEAD
       <Student/>
       <Course/>
-=======
-      <Student/> */}
-      {/* <InforCourse inforCourseData={InforCourseData} /> */}
-      <AccessCourse accessCourseData={AccessCourseData} />
->>>>>>> f247ec661b9bab9ba1cba87f857761002ab54c88
+      {/* <AccessCourse accessCourseData={AccessCourseData} /> */}
     </>
   );
 }
