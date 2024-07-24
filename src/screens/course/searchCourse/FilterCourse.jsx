a119978f705077172322dd36dbf4ae067dd15739
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-07-24 15:28:24 +0700

    filter star

--- src/screens/course/searchCourse/FilterCourse.jsx
+++ src/screens/course/searchCourse/FilterCourse.jsx
@@ -1,75 +1,247 @@
 import styled from "styled-components";
+import Accordion from "@mui/material/Accordion";
+import AccordionActions from "@mui/material/AccordionActions";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+
+import StarRating from "~/components/general/Other/StarRating";
 
 function FilterCourse(searchCourseData) {
   return (
     <FilterCourseWrapper>
-      <h2>Filter</h2>
-      <div>
-        <h3>Category</h3>
-        <ul>
-          <li>
-            <input type="checkbox" id="web" name="web" />
-            <label htmlFor="web">Web Development</label>
-          </li>
-          <li>
-            <input type="checkbox" id="mobile" name="mobile" />
-            <label htmlFor="mobile">Mobile Development</label>
-          </li>
-          <li>
-            <input type="checkbox" id="desktop" name="desktop" />
-            <label htmlFor="desktop">Desktop Development</label>
-          </li>
-          <li>
-            <input type="checkbox" id="network" name="network" />
-            <label htmlFor="network">Network Development</label>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h3>Level</h3>
-        <ul>
-          <li>
-            <input type="checkbox" id="beginner" name="beginner" />
-            <label htmlFor="beginner">Beginner</label>
-          </li>
-          <li>
-            <input type="checkbox" id="intermediate" name="intermediate" />
-            <label htmlFor="intermediate">Intermediate</label>
-          </li>
-          <li>
-            <input type="checkbox" id="advanced" name="advanced" />
-            <label htmlFor="advanced">Advanced</label>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h3>Price</h3>
-        <ul>
-          <li>
-            <input type="checkbox" id="free" name="free" />
-            <label htmlFor="free">Free</label>
-          </li>
-          <li>
-            <input type="checkbox" id="paid" name="paid" />
-            <label htmlFor="paid">Paid</label>
-          </li>
-        </ul>
-      </div>
+      <Accordion
+        sx={{
+          boxShadow: "none",
+          borderTop: "1px solid #e0e0e0",
+          fontSize: "1rem !important",
+          "&:before": {
+            display: "none",
+          },
+        }}
+        defaultExpanded
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="rating-content"
+          id="rating-header"
+          sx={{
+            padding: "0px 10px",
+            "& .MuiAccordionSummary-content": {
+              margin: 0,
+            },
+          }}
+        >
+          <h3>Rating</h3>
+        </AccordionSummary>
+        <AccordionDetails>
+          <FormControl>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="star"
+              name="radio-buttons-group"
+            >
+              <FormControlLabel
+                value="4"
+                control={<Radio />}
+                label={
+                  <div className="star-value">
+                    <StarRating rating_star={4} />
+                    <span id="number">4 and up</span>
+                  </div>
+                }
+              />
+
+              <FormControlLabel
+                value="3"
+                control={<Radio />}
+                label={
+                  <div className="star-value">
+                    <StarRating rating_star={3} />
+                    <span id="number">3 and up</span>
+                  </div>
+                }
+              />
+
+              <FormControlLabel
+                value="2"
+                control={<Radio />}
+                label={
+                  <div className="star-value">
+                    <StarRating rating_star={2} />
+                    <span id="number">2 and up</span>
+                  </div>
+                }
+              />
+            </RadioGroup>
+          </FormControl>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        sx={{
+          boxShadow: "none",
+          borderTop: "1px solid #e0e0e0",
+          fontSize: "1rem !important",
+          "&:before": {
+            display: "none",
+          },
+        }}
+        defaultExpanded
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="language-content"
+          id="language-header"
+          sx={{
+            padding: "0px 10px",
+            "& .MuiAccordionSummary-content": {
+              margin: 0,
+            },
+          }}
+        >
+          <h3>Language</h3>
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion
+        sx={{
+          boxShadow: "none",
+          borderTop: "1px solid #e0e0e0",
+          fontSize: "1rem !important",
+          "&:before": {
+            display: "none",
+          },
+        }}
+        defaultExpanded
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="method-content"
+          id="method-header"
+          sx={{
+            padding: "0px 10px",
+            "& .MuiAccordionSummary-content": {
+              margin: 0,
+            },
+          }}
+        >
+          <h3>Method</h3>
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        sx={{
+          boxShadow: "none",
+          borderTop: "1px solid #e0e0e0",
+          fontSize: "1rem !important",
+          "&:before": {
+            display: "none",
+          },
+        }}
+        defaultExpanded
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="price-content"
+          id="price-header"
+          sx={{
+            padding: "0px 10px",
+            "& .MuiAccordionSummary-content": {
+              margin: 0,
+            },
+          }}
+        >
+          <h3>Price</h3>
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        sx={{
+          boxShadow: "none",
+          borderTop: "1px solid #e0e0e0",
+          fontSize: "1rem !important",
+          "&:before": {
+            display: "none",
+          },
+        }}
+        defaultExpanded
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="program-content"
+          id="program-header"
+          sx={{
+            padding: "0px 10px",
+            "& .MuiAccordionSummary-content": {
+              margin: 0,
+            },
+          }}
+        >
+          <h3>Program</h3>
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
     </FilterCourseWrapper>
-  
-  )
+  );
 }
 
 const FilterCourseWrapper = styled.aside`
   display: flex;
   flex-direction: column;
-  gap: 2rem;
   padding: 2rem;
   background-color: #fff;
-  box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1);
   border-radius: 0.8rem;
-  max-height: 100vh;
-  overflow-y: auto;
+
+  .MuiAccordion-root {
+    margin: 0;
+  }
+
+  .MuiAccordionSummary-root {
+    margin: 0;
+  }
+
+  .css-1i7u1af-MuiPaper-root-MuiAccordion-root.Mui-expanded {
+    margin: 0;
+  }
+
+  h3 {
+    font-size: 1.6rem;
+    font-weight: 700;
+    margin: 0;
+    line-height: 1.6;
+  }
+
+  .star-value {
+    display: flex;
+    align-items: center;
+
+    span {
+      font-size: 1.6rem;
+      margin-top: 0.5rem;
+    }
+    #number {
+      margin-left: 20px;
+      line-height: 1.6;
+    }
+  }
 `;
 
 export default FilterCourse;
