200e5bc66d75ca3fb9189c5fbc6de7ef03cb930b
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-08-03 23:49:02 +0700

    chore: Update MySQL password in connMySql.js and fix linting issues in AccessCourse.jsx

--- src/screens/admin/dashboard/GeneralDashboard.jsx
+++ src/screens/admin/dashboard/GeneralDashboard.jsx
@@ -1,5 +1,165 @@
+import React from "react";
+import styled from "styled-components";
+
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import PersonalVideoIcon from "@mui/icons-material/PersonalVideo";
+import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
+import DnsIcon from "@mui/icons-material/Dns";
+
 function GeneralDashboard() {
-  return <div>GeneralDashboard</div>;
+  return (
+    <GeneralDashboardWrapper>
+      <h3>General</h3>
+      <div className="general-card-list">
+        <div className="general-card">
+          <div className="general-icon">
+            <span id="user">
+              <AccountCircleIcon style={{ fontSize: 40 }} />
+            </span>
+          </div>
+          <div className="general-content">
+            <h4>Instructor</h4>
+            <p>1000</p>
+          </div>
+        </div>
+        <div className="general-card">
+          <div className="general-icon">
+            <span id="course">
+              <PersonalVideoIcon style={{ fontSize: 40 }} />
+            </span>
+          </div>
+          <div className="general-content">
+            <h4>Courses</h4>
+            <p>1000</p>
+          </div>
+        </div>
+        <div className="general-card">
+          <div className="general-icon">
+            <span id="student">
+              <LocalLibraryIcon style={{ fontSize: 40 }} />
+            </span>
+          </div>
+          <div className="general-content">
+            <h4>Students</h4>
+            <p>1000</p>
+          </div>
+        </div>
+        <div className="general-card">
+          <div className="general-icon">
+            <span id="category">
+              <DnsIcon style={{ fontSize: 40 }} />
+            </span>
+          </div>
+          <div className="general-content">
+            <h4>Categories</h4>
+            <p>3</p>
+          </div>
+        </div>
+      </div>
+    </GeneralDashboardWrapper>
+  );
 }
+
+const GeneralDashboardWrapper = styled.section`
+  h3 {
+    font-size: 1.5rem;
+    font-weight: 600;
+    margin-bottom: 20px;
+  }
+
+  .general-card-list {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 20px;
+    .general-card {
+      display: flex;
+      justify-content: space-between;
+      padding: 1rem;
+      background: rgb(255, 255, 255);
+      border-radius: 0.75rem;
+      box-shadow: rgba(0, 0, 0, 0.14) 0rem 0.25rem 1.25rem 0rem,
+        rgba(64, 64, 64, 0.4) 0rem 0.4375rem 0.625rem -0.3125rem;
+
+      .general-content {
+        padding: 0 1rem;
+        h4 {
+          font-size: 1.6rem;
+          font-weight: 200;
+        }
+        p {
+          font-size: 2.4rem;
+          font-weight: 600;
+          color: rgb(52, 71, 103);
+        }
+      }
+      .general-icon #user {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 7rem;
+        height: 7rem;
+        opacity: 1;
+        margin-top: -25px;
+        background: linear-gradient(195deg, rgb(66, 66, 74), rgb(25, 25, 25));
+        color: rgb(255, 255, 255);
+        border-radius: 0.75rem;
+        box-shadow: rgba(0, 0, 0, 0.14) 0rem 0.25rem 1.25rem 0rem,
+          rgba(64, 64, 64, 0.4) 0rem 0.4375rem 0.625rem -0.3125rem;
+      }
+      .general-icon #course {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 7rem;
+        height: 7rem;
+        opacity: 1;
+        margin-top: -25px;
+        background: linear-gradient(
+          195deg,
+          rgb(73, 163, 241),
+          rgb(26, 115, 232)
+        );
+        color: rgb(255, 255, 255);
+        border-radius: 0.75rem;
+        box-shadow: rgba(0, 0, 0, 0.14) 0rem 0.25rem 1.25rem 0rem,
+          rgba(0, 187, 212, 0.4) 0rem 0.4375rem 0.625rem -0.3125rem;
+      }
+
+      .general-icon #student {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 7rem;
+        height: 7rem;
+        opacity: 1;
+        margin-top: -25px;
+        background: linear-gradient(
+          195deg,
+          rgb(255, 183, 77),
+          rgb(255, 109, 0)
+        );
+        color: rgb(255, 255, 255);
+        border-radius: 0.75rem;
+        box-shadow: rgba(0, 0, 0, 0.14) 0rem 0.25rem 1.25rem 0rem,
+          rgba(255, 109, 0, 0.4) 0rem 0.4375rem 0.625rem -0.3125rem;
+      }
+
+      .general-icon #category {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 7rem;
+        height: 7rem;
+        opacity: 1;
+        margin-top: -25px;
+        background: linear-gradient(195deg, rgb(0, 184, 148), rgb(0, 206, 201));
+        color: rgb(255, 255, 255);
+        border-radius: 0.75rem;
+        box-shadow: rgba(0, 0, 0, 0.14) 0rem 0.25rem 1.25rem 0rem,
+          rgba(0, 206, 201, 0.4) 0rem 0.4375rem 0.625rem -0.3125rem;
+      }
+    }
+  }
+`;
 
 export default GeneralDashboard;
--- src/screens/course/accessCourse/AccessCourse.jsx
+++ src/screens/course/accessCourse/AccessCourse.jsx
@@ -1,70 +1,72 @@
-import { GeneralHeader } from "~/components/general"
-import FooterNew from "~/components/general/Footer/FooterNew"
-import CourseBanner from "./CourseBanner"
-import MainAccessCourse from "./MainAccessCourse"
-import { useParams } from 'react-router-dom'
-import { useEffect, useState } from "react"
-import axios from "axios"
-import { useNavigate } from "react-router-dom"
+import { GeneralHeader } from "~/components/general";
+import FooterNew from "~/components/general/Footer/FooterNew";
+import CourseBanner from "./CourseBanner";
+import MainAccessCourse from "./MainAccessCourse";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Loading from "~/screens/system/Loading";
 
 function AccessCourse() {
-  const [isLoad, setIsLoad] = useState(true) //Data is loading
-  const [reload, setReload] = useState(false)
+  const [isLoad, setIsLoad] = useState(true); //Data is loading
+  const [reload, setReload] = useState(false);
   const { courseID } = useParams();
-  const [accessCourseData, setAccessCourseData] = useState()
-  const token = localStorage.getItem('token')
-  const userAuth = localStorage.getItem('userAuth')
-  const navigate = useNavigate()
+  const [accessCourseData, setAccessCourseData] = useState();
+  const token = localStorage.getItem("token");
+  const userAuth = localStorage.getItem("userAuth");
+  const navigate = useNavigate();
 
   useEffect(() => {
-    axios.get('http://localhost:3000/c/loadDetailsCourse', {
-      params: {
-        courseID
-      },
-      headers: {
-        'Token': token, // Thêm token và user vào header để đưa xuống Backend xác thực
-        'User': userAuth
-      }
-    })
-      .then(response => {
-        setAccessCourseData(response.data[0])
-        setIsLoad(false) //Data is loaded successfully
+    axios
+      .get("http://localhost:3000/c/loadDetailsCourse", {
+        params: {
+          courseID,
+        },
+        headers: {
+          Token: token, // Thêm token và user vào header để đưa xuống Backend xác thực
+          User: userAuth,
+        },
       })
-      .catch(error => {
+      .then((response) => {
+        setAccessCourseData(response.data[0]);
+        setIsLoad(false); //Data is loaded successfully
+      })
+      .catch((error) => {
         //Server shut down
-        if (error.message === 'Network Error')
-          navigate('/server-shutdown')
+        if (error.message === "Network Error") navigate("/server-shutdown");
         //Connection error
-        if (error.response.status === 500)
-          navigate('/500error')
+        if (error.response.status === 500) navigate("/500error");
         //Unauthorized. Need login
-        if (error.response.status === 401)
-          navigate('/401error')
+        if (error.response.status === 401) navigate("/401error");
         //Forbidden. Token != userAuth
-        if (error.response.status === 403)
-          navigate('/403error')
-        setIsLoad(false)
-      })
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [reload])
+        if (error.response.status === 403) navigate("/403error");
+        setIsLoad(false);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [reload]);
 
   return (
     <>
-      <GeneralHeader />
-      <main>
-        {
-          //Ràng điều kiện nếu dữ liệu đang load thì ko gọi thẻ UserProfile
-          //Vì react chạy bất đồng bộ nên chưa có dữ liệu mà gọi thẻ là sẽ bị null
-          isLoad ? ( <p>Loading...</p> ) :
-            (
-              <>
-                <CourseBanner accessCourseData={accessCourseData} />
-                <MainAccessCourse accessCourseData={accessCourseData} setReload={setReload}/>
-              </>
-            )
-        }
-      </main>
-      <FooterNew />
+      {
+        //Ràng điều kiện nếu dữ liệu đang load thì ko gọi thẻ UserProfile
+        //Vì react chạy bất đồng bộ nên chưa có dữ liệu mà gọi thẻ là sẽ bị null
+        isLoad ? (
+          <Loading />
+        ) : (
+          <>
+            <GeneralHeader />
+            <main>
+              <CourseBanner accessCourseData={accessCourseData} />
+              <MainAccessCourse
+                accessCourseData={accessCourseData}
+                setReload={setReload}
+              />
+            </main>
+            <FooterNew />
+          </>
+        )
+      }
     </>
   );
 }
