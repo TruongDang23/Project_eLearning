1a8d5115d5f7bcd39ee9a061417d4e2b819c6228
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-08-04 16:02:05 +0700

    chore: Add PaidIcon and ChatIcon to GeneralDashboard

--- src/screens/admin/dashboard/GeneralDashboard.jsx
+++ src/screens/admin/dashboard/GeneralDashboard.jsx
@@ -5,6 +5,8 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import PersonalVideoIcon from "@mui/icons-material/PersonalVideo";
 import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
 import DnsIcon from "@mui/icons-material/Dns";
+import PaidIcon from "@mui/icons-material/Paid";
+import ChatIcon from "@mui/icons-material/Chat";
 
 function GeneralDashboard() {
   return (
@@ -55,6 +57,28 @@ function GeneralDashboard() {
             <p>3</p>
           </div>
         </div>
+        <div className="general-card">
+          <div className="general-icon">
+            <span id="money">
+              <PaidIcon style={{ fontSize: 40 }} />
+            </span>
+          </div>
+          <div className="general-content">
+            <h4>Income</h4>
+            <p>1000$</p>
+          </div>
+        </div>
+        <div className="general-card">
+          <div className="general-icon">
+            <span id="review">
+              <ChatIcon style={{ fontSize: 40 }} />
+            </span>
+          </div>
+          <div className="general-content">
+            <h4>Review</h4>
+            <p>2000</p>
+          </div>
+        </div>
       </div>
     </GeneralDashboardWrapper>
   );
@@ -62,15 +86,17 @@ function GeneralDashboard() {
 
 const GeneralDashboardWrapper = styled.section`
   h3 {
-    font-size: 1.5rem;
+    font-size: 2.4rem;
     font-weight: 600;
-    margin-bottom: 20px;
+    color: rgb(52, 71, 103);
+    margin-bottom: 3rem;
   }
 
   .general-card-list {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
-    gap: 20px;
+    gap: 3rem;
+    margin-bottom: 3rem;
     .general-card {
       display: flex;
       justify-content: space-between;
@@ -158,6 +184,58 @@ const GeneralDashboardWrapper = styled.section`
         box-shadow: rgba(0, 0, 0, 0.14) 0rem 0.25rem 1.25rem 0rem,
           rgba(0, 206, 201, 0.4) 0rem 0.4375rem 0.625rem -0.3125rem;
       }
+
+      .general-icon #money {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 7rem;
+        height: 7rem;
+        opacity: 1;
+        margin-top: -25px;
+        background: linear-gradient(
+          195deg,
+          rgb(236, 64, 122),
+          rgb(216, 27, 96)
+        );
+        color: rgb(255, 255, 255);
+        border-radius: 0.75rem;
+        box-shadow: rgba(0, 0, 0, 0.14) 0rem 0.25rem 1.25rem 0rem,
+          rgba(216, 27, 96, 0.4) 0rem 0.4375rem 0.625rem -0.3125rem;
+      }
+
+      .general-icon #review {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 7rem;
+        height: 7rem;
+        opacity: 1;
+        margin-top: -25px;
+        background: linear-gradient(195deg, rgb(255, 938, 0), rgb(255, 152, 0));
+        color: rgb(255, 255, 255);
+        border-radius: 0.75rem;
+        box-shadow: rgba(0, 0, 0, 0.14) 0rem 0.25rem 1.25rem 0rem,
+          rgba(255, 152, 0, 0.4) 0rem 0.4375rem 0.625rem -0.3125rem;
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    .general-card-list {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+  @media (max-width: 576px) {
+    .general-card-list {
+      grid-template-columns: repeat(1, 1fr);
+    }
+  }
+
+  @media (max-width: 320px) {
+    .general-card-list {
+      grid-template-columns: repeat(1, 1fr);
     }
   }
 `;
