29c4c32fa4a9e1d0d65c569fcca89d66a182c83c
TruongDang23 <quangtruong050123@gmail.com>
2024-08-22 11:16:09 +0700

    Base code for FE manage account

---
+++ src/screens/admin/manageAccount/MainManage.jsx
@@ -0,0 +1,201 @@
+//This is information screen of admin
+import { GeneralFooter, GeneralHeader } from '~/components/general'
+import styled from 'styled-components'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import TeacherList from './Teacher'
+
+function ManageAccount() {
+  const token = localStorage.getItem('token')
+  const userAuth = localStorage.getItem('userAuth')
+  const [reload, setReload] = useState(false)
+  const [activeTab, setActiveTab] = useState('Tab1');
+  const navigate = useNavigate()
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+  const [pub, setPub] = useState([])
+  const [monitor, setMonitor] = useState([])
+  const [ter, setTer] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/c/getPublishCourse', {
+      headers: {
+        'Token': token, // Thêm token và user vào header để đưa xuống Backend xác thực
+        'User': userAuth
+      }
+    })
+      .then(response => {
+        setPub(response.data)
+      })
+      .catch(error => {
+        //Server shut down
+        if (error.message === 'Network Error')
+          navigate('/server-shutdown')
+        //Connection error
+        if (error.response.status === 500)
+          navigate('/500error')
+        //Unauthorized. Need login
+        if (error.response.status === 401)
+          navigate('/401error')
+        //Forbidden. Token != userAuth
+        if (error.response.status === 403)
+          navigate('/403error')
+      })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [reload])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/c/getMonitorCourse', {
+      headers: {
+        'Token': token, // Thêm token và user vào header để đưa xuống Backend xác thực
+        'User': userAuth
+      }
+    })
+      .then(response => {
+        setMonitor(response.data)
+      })
+      .catch(error => {
+        //Server shut down
+        if (error.message === 'Network Error')
+          navigate('/server-shutdown')
+        //Connection error
+        if (error.response.status === 500)
+          navigate('/500error')
+        //Unauthorized. Need login
+        if (error.response.status === 401)
+          navigate('/401error')
+        //Forbidden. Token != userAuth
+        if (error.response.status === 403)
+          navigate('/403error')
+      })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [reload])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/c/getTerminateCourse', {
+      headers: {
+        'Token': token, // Thêm token và user vào header để đưa xuống Backend xác thực
+        'User': userAuth
+      }
+    })
+      .then(response => {
+        setTer(response.data)
+      })
+      .catch(error => {
+        //Server shut down
+        if (error.message === 'Network Error')
+          navigate('/server-shutdown')
+        //Connection error
+        if (error.response.status === 500)
+          navigate('/500error')
+        //Unauthorized. Need login
+        if (error.response.status === 401)
+          navigate('/401error')
+        //Forbidden. Token != userAuth
+        if (error.response.status === 403)
+          navigate('/403error')
+      })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [reload])
+
+  return (
+    <>
+      <div>
+        <GeneralHeader />
+        <main>
+          <Container>
+            <div className="tabs">
+              <div className="tab-buttons">
+                <button
+                  className={activeTab === 'Tab1' ? 'active' : ''}
+                  onClick={() => handleTabClick('Tab1')}
+                >
+                                  Teacher
+                </button>
+                <button
+                  className={activeTab === 'Tab2' ? 'active' : ''}
+                  onClick={() => handleTabClick('Tab2')}
+                >
+                                  Student
+                </button>
+              </div>
+              <div className="tab-content">
+                {activeTab === 'Tab1' && <TeacherList course={ pub } reload={reload} setReload={setReload}/>}
+                {/* {activeTab === 'Tab2' && <MonitoringCourse course={ monitor } reload={reload} setReload={setReload}/>}
+                {activeTab === 'Tab3' && <TerminatedCourse course={ ter } reload={reload} setReload={setReload}/>} */}
+              </div>
+            </div>
+          </Container>
+        </main>
+        <GeneralFooter />
+      </div>
+    </>
+  )
+}
+
+export default ManageAccount
+const Container = styled.div`
+	.tabs {
+	font-family: 'Arial', sans-serif;
+	max-width: 70%;
+	min-width: 800px;
+	margin: 50px auto;
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+	border-radius: 8px;
+	overflow: hidden;
+	min-height: 349px;
+	max-height: 800px;
+	overflow-y: auto;
+	}
+
+	.tab-buttons {
+	display: flex;
+	width: 50px;
+	background-color: #f1f1f1;
+	}
+
+	.tab-buttons button {
+	flex: 1;
+	padding: 15px;
+	margin: 0px 5px;
+	cursor: pointer;
+	border: none;
+	border-radius: 15px;
+	background-color: #f1f1f1;
+	font-size: 1.8rem;
+	transition: background-color 0.3s, color 0.3s;
+	}
+
+	.tab-buttons button:hover {
+	background-color: #e0e0e0;
+	}
+
+	.tab-buttons button.active {
+	background-color: #187bce;
+	color: white;
+	}
+
+	.tab-content {
+	padding: 20px;
+	background-color: white;
+	}
+
+	.tab-content div {
+	animation: fadeIn 0.5s ease-in-out;
+	}
+
+	@keyframes fadeIn {
+			from {
+					opacity: 0;
+					transform: translateY(10px);
+			}
+			to {
+					opacity: 1;
+					transform: translateY(0);
+			}
+	}
+`;
---
+++ src/screens/admin/manageAccount/TeacherItem.jsx
@@ -0,0 +1,125 @@
+import styled from "styled-components";
+import { useState } from 'react'
+import { PublishCourse, RejectCourse } from '~/components/popup/index'
+import { Link } from "react-router-dom";
+
+export function Items({ courseItem, reload, setReload }) {
+  const [openPub, setopenPub] = useState(false)
+  const [openReject, setopenReject] = useState(false)
+
+  const togglePub = () => { setopenPub(!openPub) }
+  const toggleReject = () => { setopenReject(!openReject) }
+  return (
+    <>
+      <Wrapper>
+        <div key={courseItem.courseID} className="course-item">
+          <div className="image">
+            <img src={courseItem.image_introduce} alt='image' />
+          </div>
+
+          <div className="center_infor">
+            <h2>{courseItem.title}</h2>
+            <p><strong>Teacher:</strong> {courseItem.teacher}</p>
+            <p><strong>Method:</strong> {courseItem.method}</p>
+            <p><strong>Sending Date:</strong> {courseItem.time}</p>
+            <p><strong>Program:</strong> {courseItem.program}</p>
+          </div>
+
+          <div className="right_infor">
+            <h2>{courseItem.courseID}</h2>
+            <p>Monitoring</p>
+            <div className="button">
+              <Link to={`/course/infor/${courseItem.courseID}`}>
+                <button>Go to course</button>
+              </Link>
+              <button onClick={togglePub}>Accept</button>
+              <button onClick={toggleReject}>Reject</button>
+            </div>
+          </div>
+        </div>
+      </Wrapper>
+      {openPub && <PublishCourse handleClose={togglePub} course={courseItem.courseID} reload={reload} setReload={setReload} />}
+      {openReject && <RejectCourse handleClose={toggleReject} course={courseItem.courseID} reload={reload} setReload={setReload} />}
+    </>
+  )
+}
+
+const Wrapper = styled.div`
+	.course-item {
+		border: 1px solid #ccc;
+		border-radius: 8px;
+		padding: 15px;
+		margin-bottom: 15px;
+		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+
+		.image{
+    align-self: center;
+			img{
+				width: 170px;
+				height: 170px;
+				object-fit: contain;
+				border: 1px solid #ccc;
+				${"" /* bo hai góc trên của ảnh */}
+				border-top-left-radius: 20%;
+				border-top-right-radius: 20%;
+				border-bottom-left-radius: 20%;
+				border-bottom-right-radius: 20%;
+			}
+  	}
+
+		.center_infor{
+			flex: 0.4;
+
+			h2{
+				color: #187bce;
+				font-weight: bold;
+				font-size: 2.4rem;
+				padding-bottom: 5px;
+				margin-left: -30px;
+			}
+			p{
+				font-size: 1.8rem;
+			}
+		}
+
+		.right_infor{
+			flex: 0.4;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-evenly;
+
+			h2{
+				color: #187bce;
+				font-weight: bold;
+				font-size: 2.4rem;
+				padding-bottom: 5px;
+				text-align: center;
+			}
+
+			p{
+				font-size: 2.0rem;
+				text-align: center;
+			}
+
+			.button{
+				display: flex;
+				justify-content: space-around;
+
+				button{
+					font-size: 1.8rem;
+					border-radius: 15px;
+					padding: 10px;
+					border: 1px solid #ccc;
+					background-color: #f1f1f1;
+					transition: background-color 0.3s, color 0.3s;
+					&:hover {
+						background-color: #e0e0e0;
+					}
+				}
+			}
+		}
+	}
+`
