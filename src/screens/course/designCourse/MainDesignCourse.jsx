6412734f8a5f78931b4904ea7f0deb7d47902d78
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-09-11 12:21:10 +0700

    Intended learners section

--- src/screens/course/designCourse/MainDesignCourse.jsx
+++ src/screens/course/designCourse/MainDesignCourse.jsx
@@ -1,7 +1,30 @@
 import styled from 'styled-components'
 import { Element } from 'react-scroll'
+import { TextField } from '@mui/material'
+import { useState } from 'react'
+
+import AddCircleIcon from '@mui/icons-material/AddCircle'
+import CancelIcon from '@mui/icons-material/Cancel'
 
 function MainDesignCourse() {
+  // Intended learners
+  const [inputs, setInputs] = useState([
+    { value: '' },
+    { value: '' },
+    { value: '' }
+  ])
+  const handleInputChange = (index, event) => {
+    const newInputs = [...inputs]
+    newInputs[index].value = event.target.value
+    setInputs(newInputs)
+  }
+  const handleAddMore = () => {
+    setInputs([...inputs, { value: '' }])
+  }
+  const handleRemoveInput = (index) => {
+    const newInputs = inputs.filter((_, i) => i !== index)
+    setInputs(newInputs)
+  }
   return (
     <MainDesignCourseWrapper>
       <h1>Design Your Course</h1>
@@ -24,6 +47,62 @@ function MainDesignCourse() {
         <div className="design-intended">
           <h2>Intended learners</h2>
           <hr />
+          <h3>What will students learn in your course?</h3>
+          <div className="design-intended-inputs">
+            {inputs.map((input, index) => (
+              <div key={index} className="design-intended-input">
+                <TextField
+                  key={index}
+                  placeholder={`e.g. Learning Java`}
+                  value={input.value}
+                  variant="outlined"
+                  onChange={(e) => handleInputChange(index, e)}
+                  InputProps={{
+                    endAdornment: <span>200</span>,
+                    style: { fontSize: '1.6rem', color: '#555' },
+                    sx: {
+                      height: '40px',
+                      borderRadius: '5px',
+                      backgroundColor: 'rgba(243, 243, 250, 0.8)',
+                      color: '#187bce',
+                      fontSize: '1.6rem',
+                      paddingLeft: '10px',
+                      outline: 'none',
+                      transition: '0.3s all ease',
+                      '&:hover .MuiOutlinedInput-notchedOutline': {
+                        transition: '0.3s all ease',
+                        border: 'none', // Loại bỏ border khi hover
+                        boxShadow: '0 0 0 2px #187bce'
+                      },
+                      '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                        transition: '0.3s all',
+                        border: 'none', // Loại bỏ border khi focus
+                        boxShadow: '0 0 0 2px #187bce'
+                      }
+                    },
+                    notchedOutline: {
+                      border: 'none' // Loại bỏ border mặc định
+                    }
+                  }}
+                  fullWidth
+                />
+                <button onClick={() => handleRemoveInput(index)}>
+                  <span>
+                    <CancelIcon />
+                  </span>
+                </button>
+              </div>
+            ))}
+          </div>
+          <div className="design-intended-button">
+            <button onClick={handleAddMore}>
+              Add More{' '}
+              <span>
+                <AddCircleIcon />
+              </span>
+            </button>
+            <button>Save</button>
+          </div>
         </div>
       </Element>
 
@@ -48,7 +127,6 @@ const MainDesignCourseWrapper = styled.section`
   width: 100%;
   border: 1px solid #ccc;
   width: 100%;
-  height: 100vh;
   padding: 20px;
   background-color: #fff;
   border-radius: 8px;
@@ -65,6 +143,7 @@ const MainDesignCourseWrapper = styled.section`
 
   .design-general {
     margin-bottom: 20px;
+    height: 400px;
     h2 {
       font-size: 2rem;
       font-weight: 700;
@@ -81,6 +160,7 @@ const MainDesignCourseWrapper = styled.section`
 
   .design-categories {
     margin-bottom: 20px;
+    height: 400px;
     h2 {
       font-size: 2rem;
       font-weight: 700;
@@ -109,10 +189,77 @@ const MainDesignCourseWrapper = styled.section`
       height: 2px;
       background-color: #1971c2;
     }
+    h3 {
+      font-size: 1.6rem;
+      font-weight: 700;
+      color: #333;
+      margin-bottom: 10px;
+    }
+    .design-intended-inputs {
+      margin: 0 auto;
+      width: 90%;
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+
+      .design-intended-input {
+        display: flex;
+        gap: 20px;
+        align-items: center;
+        justify-content: space-between;
+        button {
+          background-color: #fff;
+          border: none;
+          svg {
+            font-size: 3rem;
+            color: #868e96;
+            transition: 0.3s all ease;
+
+            &:hover {
+              color: #e03131;
+              scale: 1.1;
+            }
+          }
+        }
+      }
+    }
+    .design-intended-button {
+      margin-top: 20px;
+      display: flex;
+      gap: 10px;
+      justify-content: flex-end;
+      button {
+        background-color: #1971c2;
+        color: #fff;
+        border: none;
+        padding: 10px 20px;
+        border-radius: 5px;
+        font-size: 1.6rem;
+        font-weight: 700;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        border: none;
+        transition: 0.3s all ease;
+        span {
+          svg {
+            font-size: 2rem;
+          }
+        }
+
+        &:hover {
+          background-color: #fff;
+          color: #187bce;
+          box-shadow: 0 0 0 2px #1971c2;
+        }
+      }
+    }
   }
 
   .design-structure {
     margin-bottom: 20px;
+    height: 400px;
     h2 {
       font-size: 2rem;
       font-weight: 700;
@@ -129,6 +276,7 @@ const MainDesignCourseWrapper = styled.section`
 
   .design-introduce {
     margin-bottom: 20px;
+    height: 400px;
     h2 {
       font-size: 2rem;
       font-weight: 700;
