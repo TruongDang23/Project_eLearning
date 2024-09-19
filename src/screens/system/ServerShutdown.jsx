04d75bb8dae091b5b7d8688f5713b5122fe868c0
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-09-19 09:21:03 +0700

    chỉnh xong title cho các page

--- src/screens/system/ServerShutdown.jsx
+++ src/screens/system/ServerShutdown.jsx
@@ -1,40 +1,62 @@
-import { useNavigate } from 'react-router-dom';
-import { Button, Typography, Container, Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom'
+import { Button, Typography, Container, Box } from '@mui/material'
+import { Helmet } from 'react-helmet' // dùng để thay đổi title của trang
 
 const ServerShutdownPage = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const handleRedirectToHome = () => {
-    navigate('/');
-  };
+    navigate('/')
+  }
 
   return (
-    <Container maxWidth="sm" sx={{ textAlign: 'center', mt: 8, mb: 8 }}>
-      <Box sx={{ mb: 4, color: '#187bce', fontSize: '120px', fontWeight: 'bold' }}>
-        Server Down
-      </Box>
-      <Typography variant="h4" gutterBottom sx={{ color: '#333', mb: 2, fontSize: '4rem' }}>
-        The server has been shut down.
-      </Typography>
-      <Typography variant="body1" paragraph sx={{ color: '#555', mb: 4, fontSize: '2rem' }}>
-        We are experiencing technical difficulties. Please try again later or contact support.
-      </Typography>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleRedirectToHome}
-        sx={{
-          fontSize: '2rem',
-          backgroundColor: '#187bce',
-          '&:hover': {
-            backgroundColor: '#155b96'
-          }
-        }}
-      >
-        Go to Homepage
-      </Button>
-    </Container>
-  );
-};
+    <>
+      <Helmet>
+        <title>Server Down | EL-Space</title>
+      </Helmet>
+      <Container maxWidth="sm" sx={{ textAlign: 'center', mt: 8, mb: 8 }}>
+        <Box
+          sx={{
+            mb: 4,
+            color: '#187bce',
+            fontSize: '120px',
+            fontWeight: 'bold'
+          }}
+        >
+          Server Down
+        </Box>
+        <Typography
+          variant="h4"
+          gutterBottom
+          sx={{ color: '#333', mb: 2, fontSize: '4rem' }}
+        >
+          The server has been shut down.
+        </Typography>
+        <Typography
+          variant="body1"
+          paragraph
+          sx={{ color: '#555', mb: 4, fontSize: '2rem' }}
+        >
+          We are experiencing technical difficulties. Please try again later or
+          contact support.
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleRedirectToHome}
+          sx={{
+            fontSize: '2rem',
+            backgroundColor: '#187bce',
+            '&:hover': {
+              backgroundColor: '#155b96'
+            }
+          }}
+        >
+          Go to Homepage
+        </Button>
+      </Container>
+    </>
+  )
+}
 
-export default ServerShutdownPage;
+export default ServerShutdownPage
--- src/screens/teacher/index.jsx
+++ src/screens/teacher/index.jsx
@@ -10,10 +10,10 @@ export function Instructor() {
       {/* Để tạo route thì cần import màn hình cần có và đặt url cho màn hình đó
       VD: /signup thì gọi ra màn hình Signup */}
       <Routes>
-        <Route path='/instructor/information' element={<Information />} />
-        <Route path='/instructor/dashboard' element={<DashboardInstructor />} />
-        <Route path='/instructor/profile' element={<ProfileInstructor />} />
-        <Route path="/instructor/manageCourse" element={<ManageCourse/>} />
+        <Route path="/instructor/information" element={<Information />} />
+        <Route path="/instructor/dashboard" element={<DashboardInstructor />} />
+        <Route path="/instructor/profile" element={<ProfileInstructor />} />
+        <Route path="/instructor/manageCourse" element={<ManageCourse />} />
       </Routes>
     </>
   )
